980cafaad98de864e02fdb8af1912c22db60abe3
ChiranjithChkz <chiranjithchakma09@gmail.com>
2025-10-05 10:56:38 +0600

    added audio voice

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,13 @@ const createElements=(arr)=>{
      return htmlElements.join(" ");
 }
 
+// for Audio------------------>
+ function pronounceWord(word) {
+      const utterance = new SpeechSynthesisUtterance(word);
+      utterance.lang = 'en-EN'; // English
+      window.speechSynthesis.speak(utterance);
+    }
+
 const manageSpinner=(status)=>{
      if(status == true){
         document.getElementById("spinner").classList.remove("hidden");
@@ -137,7 +144,7 @@ const displayLevelWord = (words) => {
 
                 <button onclick="loadWordDetails(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] "><i class="fa-solid fa-circle-info"></i></button>
 
-                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+                <button onclick="pronounceWord('${word.word}')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
               </div>
           </div>
         `;
